680ca4cfc7a396ea23ad4d676fffc179754e2878
Wangari-j <wangarijudy@gmail.com>
2024-03-20 23:37:21 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,13 @@
+for_sale = True
+
+if for_sale:
+    print("This item is for sale")
+else:
+    print("This is not for sale")
+
+online = False
+
+if online:
+    print("The user is online")
+else:
+    print("The user is not online")
